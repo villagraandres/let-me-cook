50bed1f0b1dfa088a84eefa49694c49609603a96
TheYouka <168247469+TheYouka@users.noreply.github.com>
2024-10-24 13:32:03 -0600

    Estructura base del programa

---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "articulos.h"
+
+int main()
+{
+
+    // Declaración de variables
+    int opcion;
+    
+    // Declaración de las estructuras
+    struct Articulo articulo = {};
+
+    printf("#### Bienvenido al sistema más moderno del mundo mundial ####\n");
+    do
+    {
+        do
+        {
+            printf("Ingresa una de las siguientes opciones\n");
+            printf("1) Artículos\n");
+            printf("2) Insumos\n");
+            printf("3) Mercados\n");
+            printf("4) Empleados\n");
+            printf("5) Proveedores\n");
+            printf("6) Ventas\n");
+            printf("7) Compras\n");
+            printf("8) Control de Inventario\n");
+            printf("9) Reportes\n");
+            printf("10) Salir\n\n> ");
+
+            scanf("%d",&opcion);
+
+            if (opcion < 1 || opcion > 10)
+                printf("\nIngresa un número de opción válida\n\n");
+
+            switch (opcion)
+            {
+            case 1:
+                break;
+
+            case 2:
+                break;
+            
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            case 5:
+                break;
+
+            case 6:
+                break;
+
+            case 7:
+                break;
+
+            case 8:
+                break;
+
+            case 9:
+                break;
+            
+            }
+
+
+        } while (opcion < 1 || opcion > 10);
+    
+    }while(opcion != 10);
+
+
+    return 0;
+}
