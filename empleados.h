fe51971168a70b1aaa59d0291675628b61693599
Sans_Nrj <168495014+ArtERSON@users.noreply.github.com>
2024-10-29 17:29:21 -0600

    Add files via upload

---
+++ empleados.h
@@ -0,0 +1,15 @@
+struct empleado{
+	int numero_empleado;
+	char nombre[200];
+	char RFC[13];
+	char correo_electronico[200];
+	float comision;
+	int year;
+	int mes;
+	int dia;
+	char calle[150];
+	char numero [10];
+	char colonia[100];
+	char municipio[100];
+	char estado[100];
+};
