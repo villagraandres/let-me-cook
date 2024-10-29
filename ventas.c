2a44b970be0ee423508ad06b0f9b7d7b65164123
villagraandres <villagraandres00@gmail.com>
2024-10-28 22:25:28 -0600

    Edna

--- ventas.c
+++ ventas.c
@@ -2,8 +2,10 @@
 #include <stdbool.h>
 #include "ventas.h"
 
+#include "articulos.h"
 
-bool validarExistencia(int ,int);
+
+bool validarExistencia(int ,int, FILE *);
 
 void menuVenta(){
     FILE *archivoPtr;
@@ -13,19 +15,51 @@ void menuVenta(){
     if (archivoPtr==NULL) {
         printf("No exisxte ningun mercado registrado");
     }else {
-        printf("Control de Ventas");
+        printf("\nControl de Ventas");
         printf("Numero de Mercado: ");
-        printf("Numero de Articulo: ");
-        scanf("%d",venta.numeroMercado);
-        //Ver que la clave exista en mercados
-        fseek(archivoPtr,sizeof(struct Venta)*(venta.numeroMercado-1),SEEK_SET);
-        //guardar en variable de mercado
-        //ver si es 0, poner no existe, de lo contrario guardar
-        printf("Ingrese la cantidad del Articulo");
+        do {
+            scanf("%d",&venta.numeroMercado);
+        }while (!validarExistencia(venta.numeroMercado,1,archivoPtr));
+        printf("\nNumero de Articulo: ");
+        do {
+            scanf("%d",&venta.numeroArticulo);
+        }while (!validarExistencia(venta.numeroArticulo,2,archivoPtr));
+
+        printf("\nCantidad:");
+        scanf("%d",&venta.cantidad);
+
+
+
+
+        printf("\nIngrese la cantidad del Articulo");
     }
 
 }
 //modo 1 sera validar clave mercado, modo 2 sera validar el articulo y las existencias
-bool validarExistencia(int clavef,int modo) {
+bool validarExistencia(int clavef,int modo, FILE *archivof) {
+    if(clavef<0) {
+        return false;
+    }
+    if(modo==1) {
+        fseek(archivof,sizeof(struct Venta)*(clavef-1),SEEK_SET);
+        //Ver que la clave exista en mercados
 
+        //guardar en variable de mercado
+        //ver si es 0, poner no existe, de lo contrario guardar
+
+    }else {
+        struct Articulo articulo;
+        fseek(archivof,sizeof(struct Articulo)*(clavef-1),SEEK_SET);
+        fread(&articulo,sizeof(struct Articulo),1,archivof);
+
+        if(articulo.claveArticulo==0) {
+            printf("La clave del producto no existe");
+            return false;
+        }
+
+        if(articulo.claveArticulo!=0) {
+            if(articulo)
+        }
+
+    }
 }
