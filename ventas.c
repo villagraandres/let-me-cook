2a6994b29f9e45cee697244840e9053661c0474f
villagraandres <villagraandres00@gmail.com>
2024-11-02 19:38:45 -0600

    cambios

--- ventas.c
+++ ventas.c
@@ -3,16 +3,21 @@
 #include "ventas.h"
 #include "articulos.h"
 #include "mercados.h"
+#include "empleados.h"
 #include "manejoArchivos.h"
 
 bool validarExistencia(int clave, int modo, FILE *archivoMercados, FILE *archivoArticulos);
 bool validarCantidad(int numeroArticulo, int cantidad, FILE *archivo);
 float obtenerPrecioArticulo(int claveArticulo, FILE *archivo);
+bool validarEmpleado(FILE *archivoEmpleado, int empleadoId);
+void generarFactura(struct Venta ventas[], int numVentas, float total, int empleadoId);
 
 void menuVenta() {
-    FILE *archivoMercados, *archivoArticulos;
-    struct Venta venta;
-    float precioTotal;
+    FILE *archivoMercados, *archivoArticulos, *archivoEmpleados;
+    struct Venta ventas[100]; 
+    int numVentas = 0;
+    float precioTotal = 0;
+    char respuesta;
 
     archivoMercados = fopen("mercados.dat", "rb+");
     if (archivoMercados == NULL) {
@@ -27,28 +32,70 @@ void menuVenta() {
         return;
     }
 
+    archivoEmpleados = fopen("empleados.dat", "rb+");
+    if (archivoEmpleados == NULL) {
+        printf("No hay ningún empleado registrado\n");
+        fclose(archivoMercados);
+        fclose(archivoArticulos);
+        return;
+    }
+
     printf("\nControl de Ventas\n");
 
-    printf("Número de Mercado: ");
     do {
-        scanf("%d", &venta.numeroMercado);
-    } while (!validarExistencia(venta.numeroMercado, 1, archivoMercados, archivoArticulos));
-
-    printf("\nNúmero de Artículo: ");
-    do {
+        printf("Número de Mercado: ");
         do {
-            scanf("%d", &venta.numeroArticulo);
-        } while (!validarExistencia(venta.numeroArticulo, 2, archivoMercados, archivoArticulos));
+            scanf("%d", &ventas[numVentas].numeroMercado);
+        } while (!validarExistencia(ventas[numVentas].numeroMercado, 1, archivoMercados, archivoArticulos));
+
+        printf("\nNúmero de Artículo: ");
+        do {
+            scanf("%d", &ventas[numVentas].numeroArticulo);
+        } while (!validarExistencia(ventas[numVentas].numeroArticulo, 2, archivoMercados, archivoArticulos));
 
         printf("\nIngrese la cantidad del Artículo: ");
-        scanf("%d", &venta.cantidad);
-    } while (!validarCantidad(venta.numeroArticulo, venta.cantidad, archivoArticulos));
+        do {
+            scanf("%d", &ventas[numVentas].cantidad);
+        } while (!validarCantidad(ventas[numVentas].numeroArticulo, ventas[numVentas].cantidad, archivoArticulos));
 
-    precioTotal = obtenerPrecioArticulo(venta.numeroArticulo, archivoArticulos) * venta.cantidad;
+        printf("¿Desea agregar otro artículo? (S/N): ");
+        scanf(" %c", &respuesta);
+
+        numVentas++;
+    } while (respuesta == 'S' || respuesta == 's');
+
+    do {
+        printf("Ingresa el número de empleado: ");
+        scanf("%d", &ventas[0].empleado);
+    } while (!validarEmpleado(archivoEmpleados, ventas[0].empleado));
+
+    for (int i = 0; i < numVentas; i++) {
+        precioTotal += obtenerPrecioArticulo(ventas[i].numeroArticulo, archivoArticulos) * ventas[i].cantidad;
+    }
+
     printf("El precio total de la venta es: %.2f\n", precioTotal);
-    
+
+    printf("¿Requiere factura? (S/N): ");
+    scanf(" %c", &respuesta);
+    if (respuesta == 'S' || respuesta == 's') {
+        generarFactura(ventas, numVentas, precioTotal, ventas[0].empleado);
+    }
+
     fclose(archivoArticulos);
     fclose(archivoMercados);
+    fclose(archivoEmpleados);
+}
+
+bool validarEmpleado(FILE *archivoEmpleado, int empleadoId) {
+    struct empleado empleadoInfo;
+    fseek(archivoEmpleado, sizeof(struct empleado) * (empleadoId - 1), SEEK_SET);
+    fread(&empleadoInfo, sizeof(struct empleado), 1, archivoEmpleado);
+
+    if (empleadoInfo.numero_empleado == 0) {
+        printf("El empleado no existe\n");
+        return false;
+    }
+    return true;
 }
 
 bool validarCantidad(int claveArticulo, int cantidad, FILE *archivo) {
@@ -122,4 +169,13 @@ float obtenerPrecioArticulo(int claveArticulo, FILE *archivo) {
     fread(&articulo, sizeof(struct Articulo), 1, archivo);
 
     return articulo.precio;
+}
+
+void generarFactura(struct Venta ventas[], int numVentas, float total, int empleadoId) {
+    printf("\nFactura:\n");
+    printf("Empleado ID: %d\n", empleadoId);
+    for (int i = 0; i < numVentas; i++) {
+        printf("Artículo %d: %d unidades\n", ventas[i].numeroArticulo, ventas[i].cantidad);
+    }
+    printf("Total: %.2f\n", total);
 }
